c6fc8dae4a495247c957c897cde5210721643357
Abhiram <abhiram.satha@gmail.com>
2021-11-10 23:45:06 +0000

    Finished typeWriter.js

--- typeWriter.js
+++ typeWriter.js
@@ -1,8 +1,16 @@
 const sentence = 'hello there from lighthouse labs';
 
+let x = 1000;
+
 for (const char of sentence) {
+  
   setTimeout(() => {
     process.stdout.write(char)
-  }, 1000);
+  }, x);
   //console.log('\n');
+  x += 50;
 }
+
+setTimeout(() => {
+  console.log('')
+}, x);
